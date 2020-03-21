5637a266ede6c07ed9ba1c5aed67d837c89c5eed
matienknows <52888650+matienknows@users.noreply.github.com>
2020-03-21 18:33:10 +0100

    created table

--- article/src/main/java/com/example/article/entity/article.java
+++ article/src/main/java/com/example/article/entity/article.java
@@ -1,4 +1,13 @@
 package com.example.article.entity;
 
+
+import javax.persistence.Entity;
+
+@Entity
 public class article {
+
+    private int articleId;
+    private String name;
+    private float price;
+
 }
