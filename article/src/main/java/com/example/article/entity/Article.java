2e13020a05f387ae0104646cc195e34c6a6fac74
matienknows <52888650+matienknows@users.noreply.github.com>
2020-03-21 18:49:47 +0100

    db connection

--- article/src/main/java/com/example/article/entity/Article.java
+++ article/src/main/java/com/example/article/entity/Article.java
@@ -1,13 +1,48 @@
 package com.example.article.entity;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-public class article {
+@Table(name="article")
+public class Article {
 
+    @Id
+    @GeneratedValue
     private int articleId;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="price")
     private float price;
 
+
+    public Article() {
+
+    }
+
+    public int getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- article/src/main/java/com/example/article/repo/ArticleRepository.java
+++ article/src/main/java/com/example/article/repo/ArticleRepository.java
@@ -1,4 +1,8 @@
 package com.example.article.repo;
 
-public interface ArticleRepository {
+import com.example.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+
 }
