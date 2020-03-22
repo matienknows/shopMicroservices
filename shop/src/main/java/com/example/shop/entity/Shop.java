108b875258e184929087e9acb317cbfb5e50e41d
matienknows <52888650+matienknows@users.noreply.github.com>
2020-03-22 18:44:08 +0100

    create entity

---
+++ shop/src/main/java/com/example/shop/entity/Shop.java
@@ -0,0 +1,36 @@
+package com.example.shop.entity;
+
+public class Shop {
+
+    private int articleid;
+    private int customerId;
+    private int orderId;
+
+    public Shop(){
+
+    }
+
+    public int getArticleid() {
+        return articleid;
+    }
+
+    public void setArticleid(int articleid) {
+        this.articleid = articleid;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+}
