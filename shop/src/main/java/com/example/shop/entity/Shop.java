eaa9492c86733cfe186def01d3098a9604cf3c84
matienknows <52888650+matienknows@users.noreply.github.com>
2020-03-27 14:07:20 +0100

    api for port

--- shop/src/main/java/com/example/shop/entity/Shop.java
+++ shop/src/main/java/com/example/shop/entity/Shop.java
@@ -3,11 +3,21 @@ package com.example.shop.entity;
 public class Shop {
 
     private int articleid;
+    private String articleName;
+    private float price;
     private int customerId;
     private int orderId;
-
+    private int port;
     public Shop(){
 
+    }
+
+    public Shop(int articleid, String articleName, float price, int port) {
+        super();
+        this.articleid = articleid;
+        this.articleName = articleName;
+        this.price = price;
+        this.port = port;
     }
 
     public int getArticleid() {
@@ -32,5 +42,29 @@ public class Shop {
 
     public void setOrderId(int orderId) {
         this.orderId = orderId;
+    }
+
+    public String getArticleName() {
+        return articleName;
+    }
+
+    public void setArticleName(String articleName) {
+        this.articleName = articleName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
