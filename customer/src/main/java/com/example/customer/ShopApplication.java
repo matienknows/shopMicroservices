1ee4fb3f84b03aa5f030838bbab155a643482d4c
matienknows <52888650+matienknows@users.noreply.github.com>
2020-03-29 14:16:16 +0200

    eureka client

--- customer/src/main/java/com/example/customer/ShopApplication.java
+++ customer/src/main/java/com/example/customer/ShopApplication.java
@@ -2,11 +2,13 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShopApplication {
 
 	public static void main(String[] args) {
